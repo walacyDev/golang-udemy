a4dcefa487f696dc010bbc01a1a4f17de2f57ab2
Walacy Vinicius <walacy.dev@gmail.com>
2024-09-24 11:22:08 -0300

    For com estruturas de controle

--- loop/main.go
+++ loop/main.go
@@ -3,23 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	//for i := 0; i < 5; i++ {
-	//	fmt.Println(i)
-	//}
-
-	// numero := 0
-
-	// for numero < 5 {
-	// 	fmt.Println("PASSANDO NO FOR: ", numero)
-	// 	numero++
-	// }
-
-	// for {
-	// 	fmt.Println("LOOP INFINITO")
-	// 	break
-	// }
-
 	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+	numero := 0
+
+	for numero < 5 {
+		fmt.Println("PASSANDO NO FOR: ", numero)
+		numero++
+	}
+
+	for {
+		fmt.Println("LOOP INFINITO")
+		break
+	}
+
+	for i := 0; i < 10; i++ {
 		if i == 3 {
 			fmt.Println("VALOR DO I É IGUAL A 3")
 		}
